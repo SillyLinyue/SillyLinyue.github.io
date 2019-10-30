c5b66b1ed25787de66ba4b0189efbf7b4522aed3
LinYue <1204988681@qq.com>
2019-10-30 21:55:26 +0800

    测试React打包后文件是否可用与展示

---
+++ precache-manifest.ad612fda8c17bf0eceaf4afcd5e018ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4571aad717fc22a1244f39702e9bf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e754009d1a2e9032f16",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "7da35863df32056a74ef",
+    "url": "/static/js/2.66c238d5.chunk.js"
+  },
+  {
+    "revision": "9e754009d1a2e9032f16",
+    "url": "/static/js/main.e13fb7a9.chunk.js"
+  },
+  {
+    "revision": "6d023684496f2eb1e373",
+    "url": "/static/js/runtime-main.bfebdaee.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
